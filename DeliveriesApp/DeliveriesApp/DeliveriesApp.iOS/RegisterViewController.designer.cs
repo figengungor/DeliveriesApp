5a94851f19db188fc070d7bd1ce736099922ab9a
figen <figengungor1@gmail.com>
2018-06-25 17:57:44 +0300

    Add dumb RegisterViewController -iOS

---
+++ DeliveriesApp/DeliveriesApp/DeliveriesApp.iOS/RegisterViewController.designer.cs
@@ -0,0 +1,56 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace DeliveriesApp.iOS
+{
+    [Register ("RegisterViewController")]
+    partial class RegisterViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField confirmPasswordTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField emailTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.NSLayoutConstraint passwordTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.NSLayoutConstraint registerButton { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (confirmPasswordTextField != null) {
+                confirmPasswordTextField.Dispose ();
+                confirmPasswordTextField = null;
+            }
+
+            if (emailTextField != null) {
+                emailTextField.Dispose ();
+                emailTextField = null;
+            }
+
+            if (passwordTextField != null) {
+                passwordTextField.Dispose ();
+                passwordTextField = null;
+            }
+
+            if (registerButton != null) {
+                registerButton.Dispose ();
+                registerButton = null;
+            }
+        }
+    }
+}
