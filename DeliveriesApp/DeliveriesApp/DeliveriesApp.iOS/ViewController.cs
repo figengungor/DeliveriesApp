41eb486baf620503c4d5f685373f8a63f3f92eaa
figen <figengungor1@gmail.com>
2018-06-25 13:50:59 +0300

    Add iOS native hello world

--- DeliveriesApp/DeliveriesApp/DeliveriesApp.iOS/ViewController.cs
+++ DeliveriesApp/DeliveriesApp/DeliveriesApp.iOS/ViewController.cs
@@ -6,26 +6,34 @@ namespace DeliveriesApp.iOS
 {
     public partial class ViewController : UIViewController
     {
-        int count = 1;
 
         public ViewController(IntPtr handle) : base(handle)
         {
         }
 
-        public override void ViewDidLoad()
+        //very similar to onCreate in Android
+        public override void ViewDidLoad() 
         {
             base.ViewDidLoad();
 
-            // Perform any additional setup after loading the view, typically from a nib.
-            Button.AccessibilityIdentifier = "myButton";
-            Button.TouchUpInside += delegate
-            {
-                var title = string.Format("{0} clicks!", count++);
-                Button.SetTitle(title, UIControlState.Normal);
-            };
+            helloButton.TouchUpInside += HelloButton_TouchUpInside;
+            
         }
 
-        public override void DidReceiveMemoryWarning()
+        private void HelloButton_TouchUpInside(object sender, EventArgs e)
+        {
+            //create an alert
+            var alert = UIAlertController.Create("Hello", $"Hello {nameTextField.Text}", UIAlertControllerStyle.Alert);
+
+            var cancelAction = UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, null);
+
+            alert.AddAction(cancelAction);
+
+            PresentViewController(alert, true, null); //display alert, true for animation, null for completion handler
+        }
+
+        //is going to be executed when the app consuming more memory that allowed 
+        public override void DidReceiveMemoryWarning() 
         {
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.		
