a27e04014d1cc72a1fd422951ba3373b44a0b2a9
figen <figengungor1@gmail.com>
2018-06-25 14:31:48 +0300

    Add dumb RegisterActivity - Android

---
+++ DeliveriesApp/DeliveriesApp/DeliveriesApp.Android/RegisterActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DeliveriesApp.Droid
+{
+    [Activity(Label = "RegisterActivity")]
+    public class RegisterActivity : Activity
+    {
+        EditText emailEditText, passwordEditText, confirmPasswordEditText;
+        Button registerButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Register);
+
+            emailEditText = FindViewById<EditText>(Resource.Id.registerEmailEditText);
+            passwordEditText = FindViewById<EditText>(Resource.Id.registerPasswordEditText);
+            confirmPasswordEditText = FindViewById<EditText>(Resource.Id.confirmPasswordEditText);
+            registerButton = FindViewById<Button>(Resource.Id.registerUserButton);
+
+            registerButton.Click += RegisterButton_Click;
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
--- DeliveriesApp/DeliveriesApp/DeliveriesApp.Android/Resources/Resource.designer.cs
+++ DeliveriesApp/DeliveriesApp/DeliveriesApp.Android/Resources/Resource.designer.cs
@@ -44,6 +44,9 @@ namespace DeliveriesApp.Droid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050006
+			public const int confirmPasswordEditText = 2131034118;
+			
 			// aapt resource value: 0x7f050000
 			public const int emailEditText = 2131034112;
 			
@@ -52,6 +55,15 @@ namespace DeliveriesApp.Droid
 			
 			// aapt resource value: 0x7f050003
 			public const int registerButton = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int registerEmailEditText = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int registerPasswordEditText = 2131034117;
+			
+			// aapt resource value: 0x7f050007
+			public const int registerUserButton = 2131034119;
 			
 			// aapt resource value: 0x7f050002
 			public const int signinButton = 2131034114;
@@ -71,6 +83,9 @@ namespace DeliveriesApp.Droid
 			
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Register = 2130903041;
 			
 			static Layout()
 			{
