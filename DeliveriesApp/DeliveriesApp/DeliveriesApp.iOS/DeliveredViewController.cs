27136955e4a3ac4295f411fe83b03f16fe1f777f
figen <figengungor1@gmail.com>
2018-06-28 18:25:21 +0300

    Add Lists in iOS

--- DeliveriesApp/DeliveriesApp/DeliveriesApp.iOS/DeliveredViewController.cs
+++ DeliveriesApp/DeliveriesApp/DeliveriesApp.iOS/DeliveredViewController.cs
@@ -1,13 +1,55 @@
-﻿using Foundation;
+﻿using DeliveriesApp.Model;
+using Foundation;
 using System;
+using System.Collections.Generic;
 using UIKit;
 
 namespace DeliveriesApp.iOS
 {
     public partial class DeliveredViewController : UITableViewController
     {
+        List<Delivery> delivered;
+
         public DeliveredViewController (IntPtr handle) : base (handle)
         {
+
+            delivered = new List<Delivery>();
+        }
+
+        public override async void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            delivered = await Delivery.GetDelivered();
+            //after getting data, tell tableview to reload
+            TableView.ReloadData();
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return delivered.Count;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("deliveryCell");
+
+            var delivery = delivered[indexPath.Row];
+
+            cell.TextLabel.Text = delivery.Name;
+            switch (delivery.Status)
+            {
+                case 0:
+                    cell.DetailTextLabel.Text = "Waiting delivery person";
+                    break;
+                case 1:
+                    cell.DetailTextLabel.Text = "In delivery";
+                    break;
+                case 2:
+                    cell.DetailTextLabel.Text = "Delivered";
+                    break;
+            }
+
+            return cell;
         }
     }
 }
--- DeliveriesApp/DeliveriesApp/DeliveriesApp.iOS/DeliveriesViewController.cs
+++ DeliveriesApp/DeliveriesApp/DeliveriesApp.iOS/DeliveriesViewController.cs
@@ -1,13 +1,54 @@
-﻿using Foundation;
+﻿using DeliveriesApp.Model;
+using Foundation;
 using System;
+using System.Collections.Generic;
 using UIKit;
 
 namespace DeliveriesApp.iOS
 {
     public partial class DeliveriesViewController : UITableViewController
     {
+        List<Delivery> deliveries;
+
         public DeliveriesViewController (IntPtr handle) : base (handle)
         {
+            deliveries = new List<Delivery>();
+        }
+
+        public override async void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            deliveries = await Delivery.GetDeliveries();
+            TableView.ReloadData();
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return deliveries.Count;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("deliveryCell");
+
+            var delivery = deliveries[indexPath.Row];
+
+            cell.TextLabel.Text = delivery.Name;
+            switch (delivery.Status)
+            {
+                case 0:
+                    cell.DetailTextLabel.Text = "Waiting delivery person";
+                    break;
+                case 1:
+                    cell.DetailTextLabel.Text = "In delivery";
+                    break;
+                case 2:
+                    cell.DetailTextLabel.Text = "Delivered";
+                    break;
+            }
+
+            return cell;
         }
     }
 }
